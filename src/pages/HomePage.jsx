f2575c0fede2df430005379178e8b3799cec6bf4
alimab <alimabrouki9797@gmail.com>
2025-12-15 12:05:45 +0100

    render added history and save it in local storage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,19 @@ import '../styles/Header.css'
 import '../styles/HomePage.css'
 import { Header } from '../components/Header'
 import { PromptBox } from './PromptBox'
+import { useEffect, useState } from 'react'
 
 
 export const HomePage = () => {
-  
+  const [addedHitsory, setAddedHistory] = useState(JSON.parse(localStorage.getItem('newPrompt')) ?? [])
 
+  useEffect(() => {
+    localStorage.setItem('newPrompt', JSON.stringify(addedHitsory))
+  }, [addedHitsory])
+
+  const handleAddHistory = (newPrompt) => {
+    setAddedHistory((prevHistory => [newPrompt, ...prevHistory].slice(0, 3)));
+  }
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/src/assets/images/logo.png" />
@@ -24,13 +32,15 @@ export const HomePage = () => {
             <h3 className='under-title-two'>
               jibnelk a9wa AI agents bech irivz w ye5dem m3ak as3eb les exercices
             </h3>
-           <PromptBox />
-           <div className="recent-homework">
-            <h2>Recent Homework</h2>
-            <span>ffkdsfsdkfskdfsdfsfskfsjosjfjsd</span>
-            <span>fhshfihsdifhsdfsdfhsdhfhdsuhfudsh</span>
-            <span>dsfsdfsjdfjsdjfjsdoijfosjdofjsodijfi</span>
-           </div>
+            <PromptBox handleAddHistory={handleAddHistory} />
+            <div className="recent-homework">
+              <h2>Recent Homework</h2>
+              {
+                addedHitsory.map((history, index) => (
+                  <span key={index} >{history}</span>
+                ))
+              }
+            </div>
           </div>
         </div>
       </div>
