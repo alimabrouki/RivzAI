9b83b39dfffeb6247a2d7a4f675113c61859609a
alimab <alimabrouki9797@gmail.com>
2025-12-22 14:49:32 +0100

    added FeaturesBoxes CSS File and add  icons and styled the  boxes

--- src/pages/Home/FeaturesBoxes.jsx
+++ src/pages/Home/FeaturesBoxes.jsx
@@ -1,22 +1,27 @@
-import { TrendingUp } from 'lucide-react'
+import { GraduationCap, School, TrendingUp } from 'lucide-react'
 import '../../styles/FeaturesBoxes.css'
 export const FeaturesBoxes = () => {
   return (
     <div className="features-boxes">
         <div className="box">
           <div className="feature">
-            <TrendingUp />
-            <p>AI-Powered Grade Booster for Faster HomeWork Mastery</p>
+            <TrendingUp className='icon' size={30} />
+            <h3 className="highlight-ft">AI-Powered</h3>
+            <span>Grade Booster for Faster HomeWork Mastery</span>
           </div>
         </div>
         <div className="box">
           <div className="feature">
-
+            <GraduationCap className='icon' size={30} />
+            <h3 className="highlight-ft">Teacher Mode</h3>
+            <span>Smart Homework Generator</span>
           </div>
         </div>
         <div className="box">
           <div className="feature">
-
+          <School className='icon' size={30} />
+          <h3 className="highlight-ft">100%</h3>
+          <span>Aligned With The Tunisian School Curriculum</span>
           </div>
         </div>
     </div>
