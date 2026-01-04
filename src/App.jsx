476b4afe1cbe0949b1dae233d89b569ce2aba67b
alimab <alimabrouki9797@gmail.com>
2026-01-04 14:24:17 +0100

    lift the state up to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import { Route, Routes } from 'react-router'
 import { HomePage } from './pages/Home/HomePage'
 import { HistoryPage } from './pages/History/HistoryPage'
 import './styles/index.css'
+import { useLocalStorage } from '../src/hooks/useLocalStorage'
 
 
 const App = () => {
-
+   const [addedHistory, setAddedHistory] = useLocalStorage('newPrompt', [
+      'Rewrite this paragraph simpler',
+      'Translate to Arabic / French / English',
+      'Summaries of lessons'
+    ]);
+  
+    const handleAddHistory = (newPrompt) => {
+      const historyItem = {
+        id: crypto.randomUUID(),
+        text: typeof newPrompt === 'string' ?  newPrompt : newPrompt.text ,
+        timestamp: new Date().toISOString()
+      }
+      setAddedHistory(prevHistory => [historyItem,...prevHistory ])
+      console.log(addedHistory)
+    }
 
   return (
    
       <Routes>
-        <Route index path='/' element={<HomePage />}/>
-        <Route index path='/history' element={<HistoryPage />}/>
+        <Route index path='/' element={<HomePage handleAddHistory={handleAddHistory} addedHistory={addedHistory} />}/>
+        <Route index path='/history' element={<HistoryPage addedHistory={addedHistory} />}/>
       </Routes>
     
   )
