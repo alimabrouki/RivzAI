ad23050e621284c32e14e376fe0ba02c8d4e7031
alimab <alimabrouki9797@gmail.com>
2026-01-08 14:20:28 +0100

    add a callback to make animation of typing falsy after rendering ai response

--- src/App.jsx
+++ src/App.jsx
@@ -6,58 +6,77 @@ import { useLocalStorage } from '../src/hooks/useLocalStorage'
 
 
 const App = () => {
-   const [addedHistory, setAddedHistory] = useLocalStorage('newPrompt', [
-   {
-        id: crypto.randomUUID(),
-        title: 'new homework',
-        text: 'new homework' ,
-        messages: [
-          {
+  const [addedHistory, setAddedHistory] = useLocalStorage('newPrompt', [
+    {
+      id: crypto.randomUUID(),
+      title: 'new homework',
+      text: 'new homework',
+      messages: [
+        {
+          id: crypto.randomUUID(),
           role: 'user',
           content: 'new homework'
         }
       ],
-        timestamp: new Date().toISOString()
-      }
-   ]);
-  
-    const handleAddHistory = (newPrompt) => {
-      const historyItem = {
-        id: crypto.randomUUID(),
-        title: newPrompt.slice(0,50),
-        text: typeof newPrompt === 'string' ?  newPrompt : newPrompt.text ,
-        messages: [
-          {
+      timestamp: new Date().toISOString()
+    }
+  ]);
+
+  const handleAddHistory = (newPrompt) => {
+    const historyItem = {
+      id: crypto.randomUUID(),
+      title: newPrompt.slice(0, 50),
+      text: typeof newPrompt === 'string' ? newPrompt : newPrompt.text,
+      messages: [
+        {
+          id: crypto.randomUUID(),
           role: 'user',
-          content: typeof newPrompt === 'string' ?  newPrompt : newPrompt.text
+          content: typeof newPrompt === 'string' ? newPrompt : newPrompt.text
         }
       ],
-        timestamp: new Date().toISOString()
-      }
-      setAddedHistory(prevHistory => [historyItem,...prevHistory ])
+      timestamp: new Date().toISOString()
     }
+    setAddedHistory(prevHistory => [historyItem, ...prevHistory])
+  }
 
-    const updateMessages = (cardId,message) => {
-     setAddedHistory(prev =>
-      prev.map((card) => 
-      card.id === cardId ? {
-        ...card,
-        messages:[
-          ...card.messages,
-         message
-        ]
-      } : card
+  const updateMessages = (cardId, message) => {
+    setAddedHistory(prev =>
+      prev.map((card) =>
+        card.id === cardId ? {
+          ...card,
+          messages: [
+            ...card.messages,
+            message
+          ]
+        } : card
       )
-     ) 
-    }
-  
+    )
+  }
+
+  const markMessageAnimated = (clickedCardId, msgId) => {
+    setAddedHistory(prev =>
+      prev.map((card) =>
+        card.id === clickedCardId ?
+          {
+            ...card,
+            messages: card.messages.map((m) =>
+              m.id === msgId ?
+                {
+                  ...m,
+                  animated: false
+                } : m
+            )
+          } : card
+      ))
+  }
+
   return (
-   
-      <Routes>
-        <Route index path='/' element={<HomePage handleAddHistory={handleAddHistory} addedHistory={addedHistory} />}/>
-        <Route index path='/history' element={<HistoryPage  handleAddHistory={handleAddHistory} addedHistory={addedHistory} updateMessages={updateMessages} />}/>
-      </Routes>
-    
+
+    <Routes>
+      <Route index path='/' element={<HomePage handleAddHistory={handleAddHistory} addedHistory={addedHistory} />} />
+      <Route index path='/history' element={<HistoryPage markMessageAnimated={markMessageAnimated} handleAddHistory={handleAddHistory} addedHistory={addedHistory} updateMessages={updateMessages} />} />
+    </Routes>
+
   )
 }
 
