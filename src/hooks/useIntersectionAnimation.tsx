502d1fe92b00656ab09c1716dcc1995d36d90d38
alimab <alimabrouki9797@gmail.com>
2026-01-26 10:07:11 +0100

    convert useIntersectionAnimation to TypeScript

--- src/hooks/useIntersectionAnimation.tsx
+++ src/hooks/useIntersectionAnimation.tsx
@@ -1,9 +1,9 @@
 import { useRef, useEffect } from "react";
 
-export const useIntersectionAnimation = (options = { threshold: 0.1}) => {
-  const observerRef = useRef(null);
+export const useIntersectionAnimation = (options: IntersectionObserverInit = { threshold: 0.1}) => {
+  const observerRef = useRef<IntersectionObserver| null>(null);
 
-  const observe = (element) => {
+  const observe = (element : HTMLDivElement | null) => {
     if (!element || !observerRef.current) return;
     observerRef.current.observe(element)
   };
@@ -13,11 +13,11 @@ export const useIntersectionAnimation = (options = { threshold: 0.1}) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('active');
-          observerRef.current.unobserve(entry.target);
+          observerRef.current?.unobserve(entry.target);
         }
       });
     },options);
-    return () => observerRef.current.disconnect();
+    return () => observerRef.current?.disconnect();
   },[options])
 
   return observe
