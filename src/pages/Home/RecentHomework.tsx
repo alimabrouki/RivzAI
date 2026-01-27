6ffb1876129bdec8086a888cf6a1e015a3fcb989
alimab <alimabrouki9797@gmail.com>
2026-01-27 09:54:46 +0100

    convert RecentHomework to TypeScript

--- src/pages/Home/RecentHomework.tsx
+++ src/pages/Home/RecentHomework.tsx
@@ -2,16 +2,17 @@ import '../../styles/hooks-styles/useIntersectionAnimation.css'
 import '../../styles/home-page/RecentHomework.css'
 import { Link } from 'react-router-dom'
 import { useIntersectionAnimation } from '../../hooks/useIntersectionAnimation'
+import type { HomeworkCard } from '../../types/Chat'
 
-export const RecentHomework = ({ recentHistory }) => {
+export const RecentHomework = ({ recentHistory }: {recentHistory: HomeworkCard[]}) => {
 
   const observe = useIntersectionAnimation({ threshold: 0.1 });
   return (
     <div ref={observe} className="recent-homework slide-in">
-      <h2 ref={observe} className='slide-in'>Recent Homework</h2>
+      <h2 ref={observe}  className='slide-in'>Recent Homework</h2>
       {
         recentHistory.slice(0,3).map((history) => (
-          <Link ref={observe} key={history.id}  to={`/history/${history.id}`} className='recent-prompt slide-in'><span className='slide-in' ref={observe} key={history.id} >" {history.text || history} "</span></Link>
+          <Link  key={history.id}  to={`/history/${history.id}`} className='recent-prompt slide-in'><span className='slide-in' ref={observe} >" {history.text} "</span></Link>
         ))
       }
     </div>
