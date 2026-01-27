3a3b105f81f9421823f8809bcd6419b100a448d9
alimab <alimabrouki9797@gmail.com>
2026-01-27 09:57:08 +0100

    convert useLocalStorage to TypeScript

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
+import type { HomeworkCard } from "../types/Chat";
 
-export const useLocalStorage = (key,initialValue) => {
+export const useLocalStorage = (key : string,initialValue: HomeworkCard[] | string) => {
   const [value, setValue] = useState(() => {
     const saved = localStorage.getItem(key);
     return saved ? JSON.parse(saved) : initialValue;
