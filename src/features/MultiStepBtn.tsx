bd866821f771007d906094d95f688677ff221dce
alimab <alimabrouki9797@gmail.com>
2026-01-27 09:59:28 +0100

    style: move styles to css file

--- src/features/MultiStepBtn.tsx
+++ src/features/MultiStepBtn.tsx
@@ -2,31 +2,26 @@ import '../styles/index.css'
 import '../styles/header/Header.css'
 import '../styles/home-page/HomePage.css'
 import { useState } from 'react'
-import {Dot } from 'lucide-react';
+import { Dot } from 'lucide-react';
 
 export const MultiStepBtn = () => {
   const [clicked, setClicked] = useState(false);
   const btnClicked = () => {
     setClicked(!clicked)
   }
-  const btnStyle = {
-    background: clicked ? 'linear-gradient(90deg, rgb(240, 181, 2) 17%, rgba(240, 128, 0, 1) 68%, rgba(255, 162, 0, 1) 100%)' : '',
-    color: clicked ? 'var(--c-neutral-0)' : '',
-    fontWeight: clicked ? '500' : ''
-  }
-  const dotOn = {
-    position: clicked ? 'absolute': '' ,
-    color: clicked ? 'var(--c-neutral-0)' : '',
-    right: clicked ? '-15px' : '',
-    top: clicked ? '-16px' : '' 
+ 
+  const dotOn: React.CSSProperties = {
+    position: clicked ? 'absolute' : undefined,
+    color: clicked ? 'var(--c-neutral-0)' : undefined,
+    right: clicked ? '-15px' : undefined,
+    top: clicked ? '-16px' : undefined
   }
   return (
     <button onClick={() => {
       btnClicked()
-    }} style={btnStyle} type='button' className='multi-step-mode'>
+    }} type='button' className={`multi-step-mode ${clicked ? 'multi-step-mode-on' : ''}` }>
       Multi-Step Mode
-      <Dot className='dot-on-off' style={dotOn} size={50} />
-
+      <Dot className={`dot-off ${clicked ? 'dot-off' : ''}`} style={dotOn} size={50} />
     </button>
   )
 }
