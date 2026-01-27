72d6c66d8bebfbac4d7dd04ea30c73c9a4fa34e1
alimab <alimabrouki9797@gmail.com>
2026-01-27 10:03:16 +0100

    change timestamp type from number to string

--- src/types/Chat.ts
+++ src/types/Chat.ts
@@ -3,13 +3,13 @@ export type HomeworkCard = {
   title: string;
   text: string;
   messages: Message[];
-  timestamp: number
+  timestamp: string
 }
 
 export type Message = {
   id: string;
   role: 'user' | 'ai';
   content: string;
-  animated: boolean;
-  reaction: string
+  animated?: boolean;
+  reaction?: 'like' | 'dislike' | null
 }
