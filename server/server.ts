d7822cb867374e7706e4680612cc042617c216e3
alimab <alimabrouki9797@gmail.com>
2026-06-03 10:04:02 +0100

    add userRouter to express app

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import authRouter from "./routes/auth.routes";
 import historyRouter from "./routes/history.routes";
+import userRouter from "./routes/user.routes";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use("/auth", authRouter);
+
+app.use("/users", userRouter);
 
 app.use("/history", historyRouter);
 
