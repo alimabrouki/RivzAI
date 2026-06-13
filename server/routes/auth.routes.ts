d97c9cc497adc6c4b700938fc84bad0e0f119cf6
alimab <alimabrouki9797@gmail.com>
2026-06-13 10:23:37 +0100

    add: register a user in authRouter

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
+import prisma from "../lib/prisma";
+const authRouter = Router();
 
-const authRouter = Router()
-
-authRouter.post('/register', async (req, res) => {
-  const {email, password, username} = req.body
+authRouter.post("/register", async (req, res) => {
+  const { email, password, username } = req.body;
 
   try {
-    const user = await prisma
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+    return res.status(201).json(user);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+    return res.status(503).json({
+      message: "Server Error",
+    });
   }
-})
+});
 
-
-export default authRouter
+export default authRouter;
