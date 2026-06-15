1fdfe275266aa96e25913eb3914628c864324d70
alimab <alimabrouki9797@gmail.com>
2026-06-15 14:50:46 +0100

    export prisma

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -4,4 +4,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
-export const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
