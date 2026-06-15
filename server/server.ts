1814e49456e5b42e4ac9a9880e4ff4b44e579e92
alimab <alimabrouki9797@gmail.com>
2026-06-15 14:53:09 +0100

    add: cors and dotenv to the server setup

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,20 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import authRouter from "./routes/auth.routes";
 import historyRouter from "./routes/history.routes";
 import userRouter from "./routes/user.routes";
-
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/auth", authRouter);
 
