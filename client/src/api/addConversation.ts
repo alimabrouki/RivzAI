2da62d4caa99376dc5c1d728dbd3591816346b0d
alimab <alimabrouki9797@gmail.com>
2026-07-06 18:28:26 +0100

    add: create conversation api

---
+++ client/src/api/addConversation.ts
@@ -0,0 +1,19 @@
+import { getAuthHeaders } from "../utils/getAuthheaders";
+import { API_BASE } from "./signupUser";
+
+async function addConversation(newPrompt: string) {
+  const response = await fetch(`${API_BASE}conversations`, {
+    method: "POST",
+    headers: getAuthHeaders(),
+    body: JSON.stringify({ newPrompt }),
+  });
+  const responseData = await response.json();
+  if (!response.ok) {
+    return {
+      error: responseData.error,
+    };
+  }
+  return responseData;
+}
+
+export default addConversation;
