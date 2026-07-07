5fa353594eeb38261509042716c7f4292724e9fe
alimab <alimabrouki9797@gmail.com>
2026-07-07 11:20:48 +0100

    add: call getConversations and render the data logic

--- client/src/pages/History/HomeworkCards.tsx
+++ client/src/pages/History/HomeworkCards.tsx
@@ -1,53 +1,68 @@
-import { memo, useState } from 'react';
-import { getRelativeTime } from '../../utils/getRelativeTime';
-import '../../styles/history-page/HomeworkCards.css';
-import { MoveRight } from 'lucide-react';
-import type { HomeworkCard } from '../../types/Chat';
+import { memo, useEffect, useState } from "react";
+import { getRelativeTime } from "../../utils/getRelativeTime";
+import "../../styles/history-page/HomeworkCards.css";
+import { MoveRight } from "lucide-react";
+import type { Chat } from "../../types/Chat";
+import getConversations from "../../api/getConversations";
 
 type HomeworkCardsProps = {
-  recentHomework: HomeworkCard[];
-  handleHistoryCardClick: (homework: HomeworkCard) => void
-}
+  handleHistoryCardClick: (homework: HomeworkCard) => void;
+};
 
 export const HomeworkCards = memo(
-  ({ recentHomework, handleHistoryCardClick }:HomeworkCardsProps) => {
+  ({ handleHistoryCardClick }: HomeworkCardsProps) => {
     const [limit, setLimit] = useState(10);
-
+    const [conversations, setConversations] = useState<Chat[]>([]);
     const handleLoadMore = () => {
-      setLimit(prev => prev + 10)
-    }
+      setLimit((prev) => prev + 10);
+    };
 
-    const visibleCards = recentHomework.slice(0, limit);
+    useEffect(() => {
+      async function getconvos() {
+        const result = await getConversations();
+        console.log(result);
+        setConversations(result);
+      }
+      getconvos();
+    }, []);
+
+    const visibleConversations = conversations.slice(0, limit);
 
     return (
-
       <div className="homework-cards">
-        {visibleCards.map((homework) => (
-
-          <div onClick={() => handleHistoryCardClick(homework)} data-testid='homework-card' className='homework-card' key={homework.id}>
+        {visibleConversations.map((conversation) => (
+          <div
+            onClick={() => handleHistoryCardClick(conversation)}
+            data-testid="homework-card"
+            className="homework-card"
+            key={conversation.id}
+          >
             <div className="left-line"></div>
             <div className="card-content">
-              <div className='homework-title'>{homework.title}</div>
+              <div className="homework-title">{conversation.title}</div>
               <div className="user-prompt">
-                "{homework.text}"
+                {conversation.messages.map((message) => (
+                  <p>"{message.content}"</p>
+                ))}
               </div>
-             <div className="card-details">
-               <span className='prompt-time'>{getRelativeTime(homework.timestamp)}</span>
-              <span className='view-details'>
-                <span>View Details </span>
-                <MoveRight />
-              </span>
-             </div>
+              <div className="card-details">
+                <span className="prompt-time">
+                  {getRelativeTime(conversation.createdAt)}
+                </span>
+                <span className="view-details">
+                  <span>View Details </span>
+                  <MoveRight />
+                </span>
+              </div>
             </div>
           </div>
         ))}
-        {visibleCards.length < recentHomework.length &&
+        {visibleConversations.length < conversations.length && (
           <div className="load-more">
-            <button onClick={handleLoadMore}>
-              Load More
-            </button>
-          </div>}
+            <button onClick={handleLoadMore}>Load More</button>
+          </div>
+        )}
       </div>
-    )
-  })
-
+    );
+  },
+);
