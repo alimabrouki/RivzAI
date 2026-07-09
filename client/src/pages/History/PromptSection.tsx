5a6b19913a3e42a49643fc3d3fd14a02000eb9a7
alimab <alimabrouki9797@gmail.com>
2026-07-09 14:19:58 +0100

    add: add add message callback and generic type and remove old static logic

--- client/src/pages/History/PromptSection.tsx
+++ client/src/pages/History/PromptSection.tsx
@@ -1,23 +1,29 @@
 import { useRef, useState, type ChangeEvent, type KeyboardEvent } from "react";
-import TextareaAutosize from 'react-textarea-autosize';
-import { RecordAudio } from '../../features/input-output/RecordAudio';
-import { UploadFile } from '../../features/input-output/UploadFile'
-import { SendHorizonal } from 'lucide-react';
-import '../../styles/history-page/PromptSection.css';
+import TextareaAutosize from "react-textarea-autosize";
+import { RecordAudio } from "../../features/input-output/RecordAudio";
+import { UploadFile } from "../../features/input-output/UploadFile";
+import { SendHorizonal } from "lucide-react";
+import "../../styles/history-page/PromptSection.css";
 import type { Message } from "../../types/Chat";
+import type { ActionResult } from "../../types/ActionResult";
 
 type PromptSectionProps = {
-  addMessage: (cardId: string, message: Message) => void;
-  cardId: string;
+  handleAddMessage: (
+    chatId: number,
+    message: string,
+  ) => Promise<ActionResult<Message>>;
+  chatId: number;
+  error: string;
   handleAiTyping: (state: boolean) => void;
-}
+};
 
 export const PromptSection = ({
-  addMessage,
-  cardId,
-  handleAiTyping
+  handleAddMessage,
+  chatId,
+  error,
+  handleAiTyping,
 }: PromptSectionProps) => {
-  const [isTyping, setIsTyping] = useState('');
+  const [isTyping, setIsTyping] = useState("");
 
   const promptIn = useRef<HTMLTextAreaElement | null>(null);
 
@@ -27,54 +33,58 @@ export const PromptSection = ({
     setIsTyping(e.target.value);
   };
 
+  const submitPrompt = async () => {
+    if (!isTyping.trim() || !chatId) return;
 
-  const submitPrompt = () => {
-    if (!isTyping.trim() || !cardId) return;
+    await handleAddMessage(chatId, isTyping);
 
-    addMessage(cardId, {
-      id: crypto.randomUUID(),
-      role: 'user',
-      content: isTyping
-    });
-
-    setIsTyping('');
+    setIsTyping("");
     handleAiTyping(true);
 
-    setTimeout(() => {
-      addMessage(cardId, {
-        id: crypto.randomUUID(),
-        role: 'ai',
-        content: 'You’re viewing a demo of RivzAI. The chat experience is under development and will be available soon.',
-        animated: true,
-        reaction: null
-      })
-    }, 1500);
-  }
+    setTimeout(() => {}, 1500);
+  };
 
   const onKey = (e: KeyboardEvent) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       if (e.shiftKey) return;
       if (isMobile) {
         return;
       } else {
-        e.preventDefault()
-        submitPrompt()
+        e.preventDefault();
+        submitPrompt();
       }
-
     }
-  }
+  };
 
   const focusPrompt = () => {
     promptIn.current?.focus();
-  }
+  };
   return (
     <div className="prompt-section" onClick={focusPrompt}>
-      <TextareaAutosize onKeyDown={onKey} value={isTyping} onChange={handleTextarea} ref={promptIn} className='prompt-in' minRows={1} maxRows={10} name="" id="" placeholder="Ask RivzAI" />
+      <TextareaAutosize
+        onKeyDown={onKey}
+        value={isTyping}
+        onChange={handleTextarea}
+        ref={promptIn}
+        className="prompt-in"
+        minRows={1}
+        maxRows={10}
+        name=""
+        id=""
+        placeholder="Ask RivzAI"
+      />
       <div className="prompt-btns">
         <RecordAudio />
         <UploadFile />
-        <button onClick={submitPrompt} style={{ background: isTyping ? 'var(--c-dark-orange' : '' }} className='submit-prompt'><SendHorizonal /></button>
+        <button
+          onClick={submitPrompt}
+          style={{ background: isTyping ? "var(--c-dark-orange" : "" }}
+          className="submit-prompt"
+        >
+          {error && <p>{error}</p>}
+          <SendHorizonal />
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
