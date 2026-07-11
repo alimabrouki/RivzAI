682948c234e690cf0f843a1c410dfedaaf529a11
alimab <alimabrouki9797@gmail.com>
2026-07-11 10:50:01 +0100

    add: mount auth middleware to messages routes

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.routes";
 import userRouter from "./routes/user.routes";
 import authMiddleware from "./middleware/authMiddleware";
 import chatsRouter from "./routes/chats.routes";
+import messagesRouter from "./routes/messages.routes";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -22,6 +23,8 @@ app.use("/auth", authRouter);
 app.use("/users", authMiddleware, userRouter);
 
 app.use("/chats", authMiddleware, chatsRouter);
+
+app.use("/messages", authMiddleware, messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`server runing on port ${PORT}`);
