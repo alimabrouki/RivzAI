a4ce3440335cb5b81c49aa5a7e5ded0d712839e3
alimab <alimabrouki9797@gmail.com>
2026-08-03 11:13:03 +0100

    add: update add message api function to receive chunks of ai message

--- client/src/api/addMessage.ts
+++ client/src/api/addMessage.ts
@@ -1,28 +1,34 @@
-import type { ActionResult } from "../types/ActionResult";
-import type { Message } from "../types/Chat";
 import { getAuthHeaders } from "../utils/getAuthheaders";
 import { API_BASE } from "./signupUser";
 
 async function addMessage(
   chatId: number,
   message: string,
-): Promise<ActionResult<Message>> {
-  try {
-    const response = await fetch(`${API_BASE}chats/${chatId}/messages`, {
-      method: "POST",
-      headers: getAuthHeaders(),
-      body: JSON.stringify({ message }),
-    });
+  onchunk: (chunk: string) => void,
+): Promise<void> {
+  const response = await fetch(`${API_BASE}chats/${chatId}/messages`, {
+    method: "POST",
+    headers: getAuthHeaders(),
+    body: JSON.stringify({ message }),
+  });
 
-    const responseData = await response.json();
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.error);
+  }
 
-    if (!response.ok) {
-      return { success: false, error: responseData.error };
-    }
+  if (!response.body) {
+    throw new Error("response body is missing");
+  }
 
-    return responseData;
-  } catch {
-    return { success: false, error: "Network error" };
+  const reader = response.body.getReader();
+
+  while (true) {
+    const { done, value } = await reader.read();
+    const decoder = new TextDecoder();
+    if (done) break;
+    const chunk = decoder.decode(value, { stream: true });
+    onchunk(chunk);
   }
 }
 
