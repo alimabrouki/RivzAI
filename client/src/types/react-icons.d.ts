2bad9acc6aadf5137760b8dcd1bec66cd89db185
alimab <alimabrouki9797@gmail.com>
2026-08-26 17:51:17 +0100

    refactor: add type definitions for new react-icons modules

---
+++ client/src/types/react-icons.d.ts
@@ -0,0 +1,29 @@
+declare module "react-icons/bs" {
+  import type { IconType } from "react-icons";
+
+  export const BsEye: IconType;
+  export const BsEyeSlash: IconType;
+  export const BsFillArrowLeftCircleFill: IconType;
+  export const BsFillTrash3Fill: IconType;
+}
+
+declare module "react-icons/fa" {
+  import type { IconType } from "react-icons";
+
+  export const FaUserCircle: IconType;
+  export const FaCheckCircle: IconType;
+  export const FaGithub: IconType;
+  export const FaLinkedin: IconType;
+}
+
+declare module "react-icons/fa6" {
+  import type { IconType } from "react-icons";
+
+  export const FaXTwitter: IconType;
+}
+
+declare module "react-icons/lu" {
+  import type { IconType } from "react-icons";
+
+  export const LuMail: IconType;
+}
